78472800f02cc3e7e476bfd131eb5d18fa414e2e
CHARLES DAPPER <charlespasinidapper@gmail.com>
2023-03-16 06:36:59 -0300

    add JS to project and creaate events

---
+++ index.js
@@ -0,0 +1,8 @@
+//function onClickHabilidades() {
+//    document.getElementById("section-abilities").scrollIntoView();
+//}
+
+function onClickScrollTo (id) {
+    document.getElementById(id).scrollIntoView();
+}
+
